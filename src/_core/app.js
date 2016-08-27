0d60d9c31f5fb28a99bd512ba7bf8805d33b1d0b
Alonso Mendez <alonsomendez@gmail.com>
2016-08-27 02:33:27 -0600

    [TOOLS] Added grid overlay and opacity controller

--- src/_core/app.js
+++ src/_core/app.js
@@ -1,5 +1,36 @@
 $(function() {
   'use strict';
 
+  function toggleClass(name) {
+    if($('body').hasClass(name)){
+      $('body').removeClass(name)
+    } else {
+      $('body').addClass(name)
+    }
+  }
+
+  $(document).keydown(function(e) {
+    if(e.ctrlKey) {
+      switch(e.keyCode) {
+        case 71:
+          toggleClass('grid-visible');
+          break;
+        case 79:
+          toggleClass('grid-opaque');
+          break;
+      }
+    }
+  });
+
+  toggleClass('grid-visible');
+
+  $(window).scroll(function() {
+    var top = $(window).scrollTop();
+
+    if(top  > 100) {
+      // do something
+    }
+  });
+
   console.log('main app loaded!');
 });
