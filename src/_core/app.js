e162504af232366df0e325d6afd35aa1b69deb6a
Alonso Mendez <alonsomendez@gmail.com>
2016-08-27 21:48:40 -0600

    [JS] Added grid support with CTRL+G and opacity switcher with CTRL+O

--- src/_core/app.js
+++ src/_core/app.js
@@ -1,36 +1,47 @@
-$(function() {
-  'use strict';
+$(function () {
+    'use strict';
 
-  function toggleClass(name) {
-    if($('body').hasClass(name)){
-      $('body').removeClass(name)
-    } else {
-      $('body').addClass(name)
+    var opacity = 1,
+        sectionId = 'body';
+
+    function toggleClass(name) {
+        if ($('body').hasClass(name)) {
+            $('body').removeClass(name)
+        } else {
+            $('body').addClass(name)
+        }
     }
-  }
 
-  $(document).keydown(function(e) {
-    if(e.ctrlKey) {
-      switch(e.keyCode) {
-        case 71:
-          toggleClass('grid-visible');
-          break;
-        case 79:
-          toggleClass('grid-opaque');
-          break;
-      }
-    }
-  });
+    $(document).keydown(function (e) {
+        if (e.ctrlKey) {
+            switch (e.keyCode) {
+                case 71:
+                    toggleClass('grid-visible');
+                    break;
 
-  toggleClass('grid-visible');
+                case 79:
+                    if (opacity < 1) {
+                        opacity = opacity + 0.25
+                    } else {
+                        opacity = 0.25;
+                    }
 
-  $(window).scroll(function() {
-    var top = $(window).scrollTop();
+                    $('body').css('opacity', opacity);
+                    break;
+            }
+        }
+    });
 
-    if(top  > 100) {
-      // do something
-    }
-  });
+    toggleClass('grid-visible');
 
-  console.log('main app loaded!');
+    $(document).on('click', 'a[href^="#"]', function (e) {
+        var target = $(this).attr('href');
+
+        if ($(target).length) {
+            var pos = $(target).offset().top;
+
+            e.preventDefault();
+            $('body, html').animate({scrollTop: pos});
+        }
+    });
 });
